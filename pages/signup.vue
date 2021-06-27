bd192f14429e101c1e2d3d19766bd867f2465793
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-06-27 02:36:10 +0200

    feat(pages): create login and signup

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="text-xl text-center">
-    Vous allez être redirigé sur le portail de connexion...
+    Vous allez être redirigé sur le portail d'enregistrement...
   </div>
 </template>
 
@@ -8,7 +8,7 @@
 export default {
   layout: 'minimal',
   beforeMount() {
-    window.location.href = this.$config.insameeURL + '/login?redirect=tutorat'
+    window.location.href = this.$config.insameeURL + '/signup'
   },
 }
 </script>
