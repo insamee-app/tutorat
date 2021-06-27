a929f7b41060d1b6c33319c785719b1d81cbcd63
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-06-27 02:31:22 +0200

    feat(minimal): create layout

--- layouts/minimal.vue
+++ layouts/minimal.vue
@@ -1,9 +1,7 @@
 <template>
-  <main class="mx-12 my-20">
+  <main class="mx-12 my-16">
     <NuxtLink :to="{ name: 'index' }">
-      <h1 class="w-40 mx-auto text-2xl font-bold text-center text-primary-dark">
-        T
-      </h1>
+      <InsameeIconInsamee class="w-40 mx-auto" />
     </NuxtLink>
     <Nuxt class="max-w-xs mx-auto" />
   </main>
