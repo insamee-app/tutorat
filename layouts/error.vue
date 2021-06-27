60729dd0caa666eeb3fc7dfa52663688b4dcc0e8
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-06-27 02:30:30 +0200

    feat(error): create layout

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,21 +1,12 @@
 <template>
-  <section class="mt-16 text-center">
+  <section class="text-center">
     <template v-if="error.statusCode === 404">
-      <h1 class="text-4xl font-bold text-primary-dark">404</h1>
-      <div class="mt-8">Désolé ! Une erreur est survenue.</div>
-      <InsameeAppButton empty :to="{ name: 'tutorat' }" class="block mt-4"
-        >Regarder les tutorats actuels ?</InsameeAppButton
-      >
+      <Insamee404 :error="error" :to="{ name: 'index' }"> Acceuil </Insamee404>
     </template>
     <template v-else>
-      <h1>Il semble qu'une erreur soit survenue !</h1>
-      <InsameeAppButton large class="w-full mt-8" :to="{ name: 'index' }"
-        >Accueil</InsameeAppButton
-      >
-      <InsameeAppButton border class="w-full mt-8" @click="show = !show"
-        >En savoir plus</InsameeAppButton
-      >
-      <pre v-if="show" class="mt-8 overflow-x-auto text-left">{{ error }}</pre>
+      <InsameeErrorGlobal :error="error" :to="{ name: 'index' }">
+        Accueil
+      </InsameeErrorGlobal>
     </template>
   </section>
 </template>
