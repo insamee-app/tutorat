1e1eefb9969668e18a32e14de0d0f57c8d475b87
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-06-27 02:35:08 +0200

    feat(pages): create index page

--- pages/index.vue
+++ pages/index.vue
@@ -2,57 +2,53 @@
   <div>
     <InsameeHeroBanner>
       <template #title>
-        <InsameeHeroBannerTitle
-          >Besoin d'aide dans une de tes matières ?</InsameeHeroBannerTitle
-        >
+        <InsameeHeroBannerTitle>
+          Besoin d'aide pour une de tes matières ?
+        </InsameeHeroBannerTitle>
       </template>
       <template #subtitle>
         <InsameeHeroBannerSubtitle>
-          Sur <span class="font-bold text-primary-light">Tutorat</span>, tu
-          pourras trouver des étudiants ou des professeurs qui t’offrent leur
-          aide.
+          Sur <span class="text-primary-base">Tutorat</span>, tu pourras trouver
+          des étudiants ou des professeurs qui t’offrent leur aide.
         </InsameeHeroBannerSubtitle>
       </template>
-      <template #graphic
-        ><GraphicHeroBanner class="w-full mx-auto mt-8 h-72"></GraphicHeroBanner
-      ></template>
+      <template #graphic>
+        <GraphicHeroBanner :class="heroBannerClass"></GraphicHeroBanner>
+      </template>
     </InsameeHeroBanner>
-
-    <InsameeFullContainer around>
-      <InsameeFullContainerTitle class="px-4"
-        >Tu as du temps libre pour proposer tes services
-        ?</InsameeFullContainerTitle
-      >
-      <InsameeFullContainerText class="px-4"
-        >Sur <span class="font-semibold text-primary-base">Tutorat</span>, tu
-        peux proposer tes services pour aider des intérressés
-        !</InsameeFullContainerText
-      >
-      <GraphicTutorat class="w-full" />
+    <InsameeFullContainer v-slot="{ graphicStyle }" around>
+      <div>
+        <InsameeFullContainerTitle>
+          Tu as du temps libre pour proposer tes services ?
+        </InsameeFullContainerTitle>
+        <InsameeFullContainerText class="mt-12">
+          Sur <span class="text-primary-base">Tutorat</span>, tu peux proposer
+          tes services pour aider des intérressés !
+        </InsameeFullContainerText>
+      </div>
+      <GraphicTutorat :class="graphicStyle" />
     </InsameeFullContainer>
-
-    <InsameeFullContainer around>
-      <InsameeFullContainerTitle class="px-4"
-        >Tu veux faire le cours depuis chez toi ?</InsameeFullContainerTitle
-      >
-      <GraphicCours class="w-full px-2" />
-      <InsameeFullContainerText class="px-4"
-        >Avec <span class="font-semibold text-primary-base">Tutorat</span>, tu
-        peux choisir où et comment organiser la séance, aussi bien en distanciel
-        qu’en présentiel !</InsameeFullContainerText
-      >
+    <InsameeAppHr class="my-8" />
+    <InsameeFullContainer v-slot="{ graphicStyle }" around>
+      <InsameeFullContainerTitle>
+        Tu veux faire le cours depuis chez toi ?
+      </InsameeFullContainerTitle>
+      <GraphicCours :class="graphicStyle" />
+      <InsameeFullContainerText>
+        Avec <span class="text-primary-base">Tutorat</span>, tu peux choisir où
+        et comment organiser la séance, aussi bien en distanciel qu’en
+        présentiel !
+      </InsameeFullContainerText>
     </InsameeFullContainer>
-
-    <InsameeFullContainer around>
-      <InsameeFullContainerTitle class="w-4/5 px-4 text-left"
-        >Perfectionne tes connaissances en les partageant
-        !</InsameeFullContainerTitle
-      >
-      <GraphicProjet class="w-full px-2" />
-      <InsameeFullContainerText class="px-4 text-right">
-        <span class="font-semibold text-primary-base">Tutorat</span> te permet
-        d’apprendre en faisant apprendre aux autres, alors tout le monde est
-        gagnant !
+    <InsameeAppHr class="my-8" />
+    <InsameeFullContainer v-slot="{ graphicStyle }" around>
+      <InsameeFullContainerTitle class="w-4/5 text-left">
+        Perfectionne tes connaissances en les partageant !
+      </InsameeFullContainerTitle>
+      <GraphicProjet :class="graphicStyle" />
+      <InsameeFullContainerText class="text-right self-end">
+        <span class="text-primary-base">Tutorat</span> te permet d’apprendre en
+        faisant apprendre aux autres, alors tout le monde est gagnant !
       </InsameeFullContainerText>
     </InsameeFullContainer>
   </div>
@@ -60,6 +56,10 @@
 
 <script>
 export default {
-  created() {},
+  computed: {
+    heroBannerClass() {
+      return 'flex-1 self-end mt-8 sm:w-[60%] md:m-0 md:h-[28rem]'
+    },
+  },
 }
 </script>
