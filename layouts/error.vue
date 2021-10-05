87086f898499070ec158ddb52bc1c0799fdc2fe2
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-10-05 23:53:41 +0200

    fix(404): update page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,9 +1,9 @@
 <template>
   <section class="text-center">
     <template v-if="error.statusCode === 404">
-      <Insamee404 :error="error" :to="{ name: 'tutorats' }">
+      <InsameeError404 :error="error" :to="{ name: 'tutorats' }">
         Regarder les tutorats actuels ?
-      </Insamee404>
+      </InsameeError404>
     </template>
     <template v-else>
       <InsameeErrorGlobal :error="error" :to="{ name: 'index' }">
